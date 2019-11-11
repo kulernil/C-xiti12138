6b4dd09396923a9ab350a909c16dbf8a3d0b033e
kulernil <kuler@vivaldi.net>
2019-11-11 10:43:19 +0800

    Add solution ex1 to default.

--- default/ex1/Program.cs
+++ default/ex1/Program.cs
@@ -6,6 +6,15 @@ namespace ex1
     {
         static void Main(string[] args)
         {
+            int x, y, z;
+            double average, product;
+
+            x = Int32.Parse(Console.ReadLine());
+            y = Int32.Parse(Console.ReadLine());
+            z = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The average of numbers is {average}.");
+            Console.WriteLine($"The product of numbers is {product}.");
             Console.WriteLine("Hello World!");
         }
     }
